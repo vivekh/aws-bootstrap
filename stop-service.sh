acfa0663264ae812469901bb92b309b57fdac288
Vivek Hiremath <vivek.1886@gmail.com>
2021-05-22 15:12:46 +0530

    Server changes

--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-[ -d "/home/ec2-user/app/release" ] && \
-cd /home/ec2-user/app/release && \
-npm stop
+[ -d "/home/ec2-user/app/release" ] && cd /home/ec2-user/app/release && npm stop
